04eac9bfa7e458f36cb45ca5057d9dd0a2febb31
marc <marc@symbioticsoftware.com>
2016-04-24 23:54:30 -0700

    Added computed attributes for Backbone models.

--- plugins/computed.js
+++ plugins/computed.js
@@ -11,6 +11,61 @@ function _getFunctionArgs(func) {
 	return args;
 }
 
+function _detectComputedChanges() {
+	var i, key, keys;
+	var change, changed = this.changedAttributes();
+	var newChanged = {};
+	for(change in changed) {
+		if(changed.hasOwnProperty(change) && this._computedArgs[change]) {
+			keys = this._computedArgs[change];
+			for(i = 0; i < keys.length; ++i) {
+				key = keys[i];
+				if(!newChanged[key]) {
+					newChanged[key] = this.get(key);
+					this.trigger('change:' + key, this, newChanged[key], {});
+				}
+			}
+		}
+	}
+	// To not interefer with the current change event, use setTimeout to modify the changed object
+	setTimeout(function() {
+		this.changed = newChanged;
+		this.trigger('change', this, {});
+	}.bind(this), 0);
+}
+
+function _wrapComputedFunc(func, args) {
+	return function() {
+		var i, values = [];
+		for(i = 0; i < args.length; ++i)
+			values.push(this.get(args[i]));
+		return func.apply(this, values);
+	};
+}
+
+// Extend computed attribute capabilities to Backbone models
+Backbone.Model.prototype.setComputed = function(key, func) {
+	var i, arg, args = _getFunctionArgs(func);
+	if(!this._computed) {
+		this._computed = {};
+		this._computedArgs = {};
+		this.get = function(attr) {
+			var compFun = this._computed[attr];
+			if(compFun)
+				return compFun.call(this);
+			return Backbone.Model.prototype.get.call(this, attr);
+		};
+		this.listenTo(this, 'change', _detectComputedChanges);
+	}
+	this._computed[key] = _wrapComputedFunc(func, args);
+	for(i = 0; i < args.length; ++i) {
+		arg = args[i];
+		if(!this._computedArgs[arg])
+			this._computedArgs[arg] = [];
+		this._computedArgs[arg].push(key);
+	}
+};
+
 function _createArgObserver(key, getFunc, args) {
 	return function() {
 		var i, values = [];
@@ -21,7 +76,7 @@ function _createArgObserver(key, getFunc, args) {
 }
 
 Backbone.Cord.plugins.push({
-	name: 'calculated',
+	name: 'computed',
 	initialize: function() {
 		// Enumerate all of the get properties to determine which has a get method with arguments
 		if(this.properties) {
@@ -32,7 +87,6 @@ Backbone.Cord.plugins.push({
 					if(prop && prop.get) {
 						args = _getFunctionArgs(prop.get);
 						if(args.length) {
-							console.log(key + ' is calculated');
 							// The observer method then will call this._setKey(argValues...);
 							observer = _createArgObserver(key, prop.get, args);
 							for(i = 0; i < args.length; ++i)
